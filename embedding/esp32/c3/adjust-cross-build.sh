7b372afd6a95520c8871de12c37722cbf2ed029f
Claus Prüfer <pruefer@webcodex.de>
2026-08-08 15:02:32 +0200

    Delete

--- embedding/esp32/c3/adjust-cross-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp ./riscv32-cross.cmake.tpl ./riscv32-cross.cmake
-sed -i "s#\[HOMEDIR\]#${HOME}#g" ./riscv32-cross.cmake
--- embedding/esp32/s3/adjust-cross-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp ./xtensa-cross.cmake.tpl ./xtensa-cross.cmake
-sed -i "s#\[HOMEDIR\]#${HOME}#g" ./xtensa-cross.cmake
